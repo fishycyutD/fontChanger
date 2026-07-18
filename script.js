f6276cfdd067cb59b7d5193731a60aca1ea811cd
fishycyutD <mehann801977@gmail.com>
2026-07-18 16:40:37 +0800

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-/*chrome.action.onClicked.addListener(tab => {
-    chrome.scripting.executeScript({
-        target : {tabId : tab.id},
-        func : () => {alert("Time isn't endless. Use of this to be who you want to be. Be a robotics scientist. Be a friendly person.");}
-    });
-});*/
